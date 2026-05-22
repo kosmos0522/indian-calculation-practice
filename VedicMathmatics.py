9879776fe2f8017cac4e4a35fe2ad26e03bfd1e6
kosmos0522 <kosmos.to.study@gmail.com>
2026-05-22 11:14:20 +0900

    first commit

---
+++ VedicMathmatics.py
@@ -0,0 +1,75 @@
+import random
+
+# 掛け算の処理
+def same_tens_times(tens):
+    count = 1
+    while True:
+        #　片方の数が11,片方が10の位の数の掛け算
+        if tens == 11:
+            li = [tens, random.randint(10, 19)]
+            random.shuffle(li)
+            x = li[0]
+            y = li[1]
+
+        # 十の位が同じで1の位の合計が10の数の掛け算
+        elif tens == 10:
+            tens_value = random.randint(1, 9) * tens
+            first_ones_value = random.randint(1, 9)
+            second_ones_value = 10 - first_ones_value
+            x = tens_value + first_ones_value
+            y = tens_value + second_ones_value
+            
+        # 十の位が同じ数同士の掛け算
+        else:               
+            x = random.randint(tens * 10, tens * 10 + 9)
+            y = random.randint(tens * 10, tens * 10 + 9)
+                
+        print(f"""{count}問目
+{x}×{y}=""")
+        correct_answer = x * y
+        my_answer = input()
+
+        if my_answer == str(correct_answer):    #ここをint(correct_answer)にして全角の数値でも正解になるようにバリデーションする。
+            print("正解！")
+        else:
+            print(f"不正解！正解は{correct_answer}です。")
+        
+        print("続ける→Enter モード選択に戻る→b")
+        command = input()
+        if command == "b":
+            print()
+            return
+
+        count += 1
+
+#　＝＝メイン画面＝＝
+another = ""
+
+print("""---インド式計算のかけ算編---
+始めるにはEnterを押してください。""")
+input()
+
+while True:
+    try:
+        print(f"""{another}数値を入力して計算するモードを選択してください。
+１～９・・・１０の位が１～９同士の掛け算　例 １３×１５など
+１０・・・１０の位が同じで１の位の合計が１０の数の掛け算　例 ２３×２７など
+１１・・・片方が１１，片方が１０の位の数の掛け算　例　１１×１５など""")
+
+        tens = int(input()) 
+        if tens in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]:
+            print()
+            same_tens_times(tens)
+            another = ""
+            continue
+
+        else:
+            print("そのモードは存在しません。")
+
+
+    except ValueError:
+        print("数値を入力してください。")
+
+    print()
+    another = "もう一度"
+    continue
